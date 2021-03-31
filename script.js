b01cc1d40036986d4a0a62e26face2998bcdd50e
isaacionescu <isaac.ionescu@gmail.com>
2021-03-31 05:31:20 +0100

    v. 1.1
    - the program is now able to accept and interpret continuous digits into evergrowing live numbers
    - periods (.) as well: it integrates them into said number;
    - removed the form input box for now, for simplicity.

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let curVal = 0;
 let newVal = 0;
 let newValStr = "";
 
+let newOperator = "";
+
 class App {
 	constructor() {
 		this.addEventListeners()
@@ -30,13 +32,37 @@ class App {
 	determineButton() {
 		if(event.target.matches('.sq')) {
 			const whichButton = event.target.closest('.sq')
+			const rawValue = whichButton.dataset.id;
+			console.log(`Raw value:     ${rawValue}`)
+			console.log(`Type of value: ${typeof rawValue}`)
 
-			const whatIsIt = whichButton.dataset.id;
-			if(typeof whatIsIt )
+			const curatedVal = parseInt(rawValue)
+			console.log(`Curated value: ${curatedVal}`)
 
-			newVal = whichButton.dataset.id;
-			console.log(newVal)
-			test.innerHTML = newVal;
+			if(curatedVal.toString() != 'NaN' || rawValue == '.') {
+				console.log('You are inputting a number')
+				newValStr += rawValue;
+
+				test.innerText = newValStr
+
+				newVal = parseFloat(newValStr)
+				console.log(`Live number: ${newVal}`)
+				// console.log(`Test doubling: ${newVal * 2}`)
+
+			}
+
+			else if ((rawValue == '+' || '-' || '*' || "/")) {
+				console.log(`You are typing an operator`)
+
+				newOperator = rawValue
+				console.log(newOperator)
+
+				curVal = newVal;
+				console.log(`Current value: ${curVal}`)
+
+				newValStr = ""
+			}
+
 		}
 	}
 
