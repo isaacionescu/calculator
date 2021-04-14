96231ec0ed71b944ad037555638115befeda7577
isaacionescu <isaac.ionescu@gmail.com>
2021-04-14 02:59:09 +0100

    v. 1.4
    • UX/CSS improvements: now buttons are hoverable and highlitable + display box shows the full history of calculations;
    • JS: improvements, clarity and more separation;
    • for v. 1.3 I had forgotten to mention that I had restarted the whole JS from scratch in a new file, which is script2.js. This is the one I'll be using from now;
    • recap / reminder about fixed bugs that applies for both v. 1.3 and 1.4:
    => I had had wrong conditionals in 2 spots: if(a == b || c || d) was an incorrect syntax for achieving what I wanted 🤡;
    => previously, I had thought that not using a strong "else if" statement (as opposed to just "if") was the root for my problems with not entering the correct conditional chain. As such, turning every single branch into an "else if", as well as changing their order (eg. I had moved the "clear" conditional first for v. 1.3) only worked as a bruteforce fix based on the order of the truthy/falsy evaluation order;
    => a stupid syntax issue that I had been keeping up to v. 1.3:  checking whether the re-stringified value was a number or not, with the expression:
    stringifiedVal !== NaN
    , instead of evaluating stringifiedVal to the string "NaN" 🤡🤡🤡

--- script2.js
+++ script2.js
@@ -4,6 +4,7 @@ let valueA = 0;
 let valueB = 0;
 let rawVal = "";
 let valueStr = "";
+let operatorsArr = ['+', '-', '*', '/'];
 
 // state management
 let alreadyTypedAnOperator = false;
@@ -18,6 +19,14 @@ class App {
 		document.body.addEventListener("click", event => {
 			this.determineButton()
 		})
+
+		document.body.addEventListener("mouseover", event => {
+			this.highlight()
+		})
+
+		document.body.addEventListener("mouseout", event => {
+			this.unhighlight()
+		})
 	}
 
 	determineButton() {
@@ -29,63 +38,93 @@ class App {
 			let stringifiedVal = numberifiedVal.toString()
 			let isNumber = stringifiedVal !== "NaN";
 
-			// IF IT'S CLEAR
-			if(rawVal == "clear") {
-				valueA = 0;
-				valueB = 0;
-				valueStr = "";
-				box.innerText = "Type again :)";
-				console.log(`Value A right now: ${valueA}`)
-				console.log(`Value B right now: ${valueB}`)
-				console.log(`// You typed "clear". 
-				valueStr: ${valueStr}`)
-			}
 
 			// IF IT'S A DOT
-			else if (rawVal == ".") {
+			if (rawVal == ".") {
 				valueStr = valueStr.includes('.') ? valueStr : valueStr + rawVal;
 				box.innerText = valueStr
-				console.log(`Value A right now: ${valueA}`)
-				console.log(`Value B right now: ${valueB}`)
-				console.log(`// You typed a dot. 
-				valueStr: ${valueStr}`)
+				console.log(`Value A: ${valueA}`)
+				console.log(`Value B: ${valueB}`)
+				console.log(`// You typed a dot. valueStr: ${valueStr}`)
 			}
 
 
-			// IF IT'S A NUMBER
+			// IF IT'S A DIGIT
 			else if (isNumber) {
+				// valueB = eval(valueStr)
 				valueA = valueB;
 				valueStr+= rawVal;
 
+				box.innerText = valueStr;
+				console.log(`Value A: ${valueA}`)
+				console.log(`Value B: ${valueB}`)
+				console.log(`Last character: ${valueStr.slice(-1)}`)
+				console.log(`// You typed a digit. valueStr: ${valueStr}`)
+			}
+
+
+			// IF IT'S AN OPERATOR
+			else if (operatorsArr.includes(rawVal)) {
+				// alreadyTypedAnOperator = true;
+
+				console.log(`valueStr: ${valueStr}`)
+				let lastChar = valueStr.slice(-1);
+				console.log(`Last prev character:  ${lastChar}`)
+
+				if(operatorsArr.includes(lastChar)) {
+					console.log(`Last character IS an operator!`)
+					valueStr = valueStr.slice(0, -1);
+					valueStr+= rawVal;
+					console.log(`valueStr: ${valueStr}`)
+
+				} else {
+					console.log(`Last character is NOT operator!`)
+					valueStr+= rawVal;
+					console.log(`valueStr: ${valueStr}`)
+				}
+					
+				// valueStr = `${valueA}${rawVal}${valueB}`
+
+				valueB = parseInt(valueStr.slice(0, -1))
 				box.innerText = valueStr
-				console.log(`Value A right now: ${valueA}`)
-				console.log(`Value B right now: ${valueB}`)
-				console.log(`// You typed a digit. 
-				valueStr: ${valueStr}`)
+				console.log(`Value A: ${valueA}`)
+				console.log(`Value B: ${valueB}`)
+				console.log(`// You typed an operator. valueStr: ${valueStr}`)
 			}
 
 			// IF IT'S EQUAL
 			else if(rawVal == "=") {
 				valueB = eval(valueStr);
 				box.innerText = `${valueB}`
-				console.log(`Value A right now: ${valueA}`)
-				console.log(`Value B right now: ${valueB}`)
-				console.log(`// You typed an equal. 
-				valueStr: ${valueStr}`)
+				console.log(`Value A: ${valueA}`)
+				console.log(`Value B: ${valueB}`)
+				console.log(`// You typed an equal. valueStr: ${valueStr}`)
 			}
 
-			// IF IT'S AN OPERATOR
-			else if (rawVal == '+' || '-' || '*' || '/') {
-				alreadyTypedAnOperator = true;
-				valueB = eval(valueStr);
-				valueStr+= rawVal;
-				// valueStr = `${valueA}${rawVal}${valueB}`
-
-				console.log(`Value A right now: ${valueA}`)
-				console.log(`Value B right now: ${valueB}`)
-				console.log(`// You typed an operator. 
-				valueStr: ${valueStr}`)
+			// IF IT'S CLEAR
+			else if(rawVal == "c") {
+				valueA = 0;
+				valueB = 0;
+				valueStr = "";
+				box.innerText = "Type again :)";
+				console.log(`Value A: ${valueA}`)
+				console.log(`Value B: ${valueB}`)
+				console.log(`// You typed "clear". valueStr: ${valueStr}`)
 			}
+		}
+	}
+
+	highlight() {
+		const button = event.target.closest('.square')
+		if(event.target.matches('.square')) {
+			button.classList.add('highlight')
+		} 
+	}
+
+	unhighlight() {
+		const button = event.target.closest('.square')
+		if(event.target.matches('.square')) {
+			button.classList.remove('highlight')
 		}
 	}
 }
