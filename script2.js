405f4a9c04c9d4f01a3e94d8414f9d41b7076e47
isaacionescu <isaac.ionescu@gmail.com>
2021-04-19 01:59:31 +0100

    v. 1.42 - Finally works correctly (except decimals)

--- script2.js
+++ script2.js
@@ -6,18 +6,26 @@ let valueAstr = "";
 let valueB = 0;
 let valueBstr = "";
 
+let valueC = 0;
+
+let op = "";
+
 
 let rawVal = "";
 
-let valueStr = "";
+let fullStr = "";
 let operatorsArr = ['+', '-', '*', '/'];
 
 // state management
-let firstTimeTypingOp = true;
 let state1 = true;
-// state1  = currently, a number is being typed (digit or period)
-// !state2 = currently, an operator is being typed (+ - * / = or Clear)
+// state1  = currently a number is being typed (digit and/or period)
+// !state2 = currently an operator is being typed (+ - * / = or Clear)
 
+let virgin = true; // checks to see this is the first time user is typing a number
+
+let counter = 0;
+
+let resetBothBC = false; // keeps track of each time we reset B and C back to 0;
 
 class App {
 	constructor () {
@@ -50,120 +58,134 @@ class App {
 
 			// IF IT'S A DOT
 			if (rawVal == ".") {
-				console.log(`Operator is being typed? ${state1}`)
+				state1 = true;
 
 				// checks if the string already has a period - if yes, ignore it
-				valueStr = valueStr.includes('.') ? valueStr : valueStr + rawVal;
-				box.innerText = valueStr;
+				fullStr = fullStr.includes('.') ? fullStr : fullStr + rawVal;
+				box.innerText = fullStr;
 				console.log(`Value A: ${valueA}`)
 				console.log(`Value B: ${valueB}`)
-				console.log(`You typed a dot.`)
-				console.log(`// Total valueStr: ${valueStr}`)
+				// console.log(`You typed a dot.`)
+				console.log(`// Total fullStr: "${fullStr}"`)
 			}
 
 
 			// IF IT'S A DIGIT
 			else if (isNumber) {
-				if(!state1) {
-					state1 = true;
-					valueAstr= "";
-				} else if (state1) {
-					valueAstr = valueAstr;	
+				state1 = true;
+				console.log(`// counter:     ${counter}`)
+				console.log(`// state1:      ${state1}`)
+				console.log(`// virgin?      ${virgin}`)
+
+
+				if(virgin) {
+					valueAstr+= rawVal 
+					// console.log(`Value A string: ${valueAstr}`)
+					valueA = parseFloat(valueAstr)
+				}  else if(!virgin) {
+					valueBstr+= rawVal
+					valueB = parseFloat(valueBstr)
+					// console.log(`Value B string: ${valueBstr}`)
+					valueC = eval(`${valueA}${op}${valueB}`)
 				}
 
-				valueAstr+= rawVal;
-				console.log(`valueAstr right now: ${valueAstr}`)
 
-				valueA = parseFloat(valueAstr)
+				fullStr+= rawVal;
 
-				valueStr+= rawVal;
-				if(!firstTimeTypingOp) {
-					valueB = eval(valueStr)
-				}
-
-				console.log(`Operator is being typed? ${state1}`)
-
-				box.innerText = valueStr;
-				console.log(`Value A: ${valueA}`)
-				console.log(`Value B: ${valueB}`)
-				console.log(`Last character in valueStr: ${valueStr.slice(-1)}`)
-				console.log(`// You typed a digit.`)
-				console.log(`// Total valueStr: ${valueStr}`)
-
+				box.innerText = fullStr;
+				console.log(`Value A:        ${valueA}`)
+				console.log(`Value B:        ${valueB}`)
+				console.log(`Operator:       ${op}`)
+				console.log(`Value C:        ${valueC}`)
+				// console.log(`Last character in fullStr: ${fullStr.slice(-1)}`)
+				// console.log(`// You typed a digit.`)
+				console.log(`//// Total fullStr: "${fullStr}"`)
+				
 			}
 
 
 			// IF IT'S AN OPERATOR
 			else if (operatorsArr.includes(rawVal)) {
-				console.log(`valueStr: ${valueStr}`)
-				console.log(`Last prev character:  ${valueStr.slice(-1)}`)
-
-				state1 = true;
-				console.log(`Operator is being typed? ${state1}`)
-
-				// checks if last character of previous string is already an operator
-				// and prevents multiple operators being added
-				if(operatorsArr.includes(valueStr.slice(-1))) {
-					console.log(`Last character IS an operator!`)
-					valueStr = valueStr.slice(0, -1);
-					valueStr+= rawVal;
-					console.log(`valueStr: ${valueStr}`)
-
-				} else {
-					console.log(`Last character is NOT operator!`)
-					valueStr+= rawVal;
-					console.log(`valueStr: ${valueStr}`)
-				}
-				
-				let valueZ = 0;
-				valueB = eval(valueStr.slice(0, -1))
-
-				box.innerText = firstTimeTypingOp ? valueStr : valueStr;
-				console.log(`Value A: ${valueA}`)
-				console.log(`Value B: ${valueB}`)
-				console.log(`// You typed an operator.`)
-				console.log(`// Total valueStr: ${valueStr}`)
-				firstTimeTypingOp = false;		
 				state1 = false;
 
-				// let newFinalResult = parseFloat(`${valueA}${rawVal}${valueB}`)
-				console.log(`// New final result: ${valueB}`)
+
+				// checks if last character is already an operator, and prevents multiple operators
+				if(operatorsArr.includes(fullStr.slice(-1))) {
+					// console.log(`Last character IS an operator!`)
+					fullStr = fullStr.slice(0, -1);
+					fullStr+= rawVal;
+
+				} else {
+					// console.log(`Last character is NOT operator!`)
+					counter+= 1;
+					virgin = false;
+					fullStr+= rawVal;
+				}
+				op = fullStr.slice(-1)
+
+				if(counter > 1) {
+					valueA = valueC;
+					valueBstr = "";
+					valueB = 0;
+				}
+
+
+				console.log(`// counter:     ${counter}`)
+				console.log(`// state1:      ${state1}`)
+				console.log(`// virgin?      ${virgin}`)
+				// console.log(`// isNumber?    ${isNumber}`)
+				console.log(`fullStr:       "${fullStr}"`)
+				console.log(`Last prev character:  ${fullStr.slice(-2, -1)}`)
+
+
+				// box.innerText = firstTimeTypingOp ? fullStr : fullStr;
+				console.log(`Value A:  ${valueA}`)
+				console.log(`Operator: ${op}`)
+				console.log(`Value B:  ${valueB}`)
+				console.log(`Value C:  ${valueB}`)
+				// console.log(`// You typed an operator.`)
+				console.log(`//// Total fullStr: "${fullStr}"`)	
+				// console.log(`// New final result: ${valueB}`)
 			}
 
 			// IF IT'S EQUAL
 			else if(rawVal == "=") {
-				valueStr = (
-						operatorsArr.includes(valueStr.slice(-1)) ?
-						valueStr.slice(0, -1) : valueStr
-					)
 				state1 = false;
-				console.log(`Operator is being typed? ${state1}`)
+				fullStr = (
+						operatorsArr.includes(fullStr.slice(-1)) ?
+						fullStr.slice(0, -1) : fullStr
+					)
+				console.log(`// state1: ${state1}`)
+				console.log(`// You typed an equal`)
 
-
-
-				valueB = eval(valueStr);
-				box.innerText = `${valueB}`
+				box.innerText = valueC;
 				console.log(`Value A: ${valueA}`)
 				console.log(`Value B: ${valueB}`)
-				console.log(`// You typed an equal.`)
-				console.log(`// Total valueStr: ${valueStr}`)
+				console.log(`Value C: ${valueC}`)
+				console.log(`// Total fullStr: "${fullStr}"`)
 			}
 
 			// IF IT'S CLEAR
 			else if(rawVal == "c") {
 				state1 = false;
-				console.log(`Operator is being typed? ${state1}`)
+				console.log(`// state1: ${state1}`)
+				console.log(`// You typed CLEAR`)
+
 
 				valueA = 0;
 				valueB = 0;
-				valueStr = "";
-				firstTimeTypingOp = true;
+				valueC = 0;
+				valueAstr = "";
+				valueBstr = "";
+				fullStr = "";
+				virgin = true;
+				counter = 0;
 				box.innerText = "Type again :)";
 
 				console.log(`Value A: ${valueA}`)
 				console.log(`Value B: ${valueB}`)
-				console.log(`// You typed "clear".`)
-				console.log(`// Total valueStr: ${valueStr}`)
+				console.log(`Value C: ${valueC}`)
+				console.log(`// Total fullStr: ${fullStr}`)
 			}
 		}
 	}
